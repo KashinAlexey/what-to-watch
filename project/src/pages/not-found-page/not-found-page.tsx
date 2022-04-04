ab5ef6193b78bde9733314edee8e782ed75d1cc8
Alexey <apple230379@gmail.com>
2022-04-04 17:28:00 +0300

    Update not found page

--- project/src/pages/not-found-page/not-found-page.tsx
+++ project/src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,23 @@
+import Logo from '../../components/logo/logo';
+
 function NotFoundPage(): JSX.Element {
   return (
-    <div >404 Not found</div>
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <Logo />
+      </header>
+
+      <div style={{
+        display: 'block',
+        fontSize: '50px',
+        position: 'absolute',
+        top: '40%',
+        left: '45%',
+        marginRight: '-50%'}}
+      >
+        <p>404 Not found</p>
+      </div>
+    </div>
   );
 }
 
